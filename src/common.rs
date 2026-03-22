43e917a8f0b71d0d9a72d61c953477f49da55818
wuydshdo <yuhuiyu46@gmail.com>
2026-03-22 11:56:18 +0800

    内置服务端信息

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://43.254.167.239:21114".to_owned()
 }
 
 #[inline]
