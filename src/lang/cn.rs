a988bd8a67f121e0a3d871ed4895d5272289ec9b
wuydshdo <yuhuiyu46@gmail.com>
2026-03-23 20:40:55 +0800

    修改大牛

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -2,8 +2,8 @@ lazy_static::lazy_static! {
 pub static ref T: std::collections::HashMap<&'static str, &'static str> =
     [
         ("Status", "状态"),
-        ("Your Desktop", "你的桌面"),
-        ("desk_tip", "你的桌面可以通过下面的 ID 和密码访问。"),
+        ("Your Desktop", "大牛的桌面"),
+        ("desk_tip", "大牛的桌面可以通过下面的 ID 和密码访问。"),
         ("Password", "密码"),
         ("Ready", "已经接入大牛服务器"),
         ("Established", "已建立"),
